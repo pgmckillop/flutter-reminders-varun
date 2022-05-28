5a9cedf726be07f4cf854729311fbbc1daf92117
Paul McKillop <pgmckillop@gmail.com>
2022-05-28 18:18:17 +0100

    Lecture 57 End
    FutureBuilder added for Firebase connection.

--- lib/main.dart
+++ lib/main.dart
@@ -21,98 +21,67 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  bool _initialized = false;
-  bool _error = false;
-
-  initializeFirebase() async {
-    try {
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-        print('Firebase initialized');
-      });
-    } catch (e) {
-      setState(() {
-        _error = true;
-      });
-    }
-
-    // Firebase.initializeApp().then(
-    //   (value) {
-    //     setState(() {
-    //       _initialized = true;
-    //       //print('Initialized');
-    //     });
-    //   },
-    // ).catchError((e) {
-    //   setState(() {
-    //     _error = true;
-    //   });
-    // });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initializeFirebase();
-  }
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    if (_error) {
-      return Center(
-        child: Text('There was an error'),
-      );
-    }
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text("There was an error"),
+          );
+        }
 
-    if (!_initialized) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    return ChangeNotifierProvider<TodoListCollection>(
-      create: (BuildContext context) => TodoListCollection(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (context) => HomeScreen(),
-          '/addList': (context) => const AddListScreen(),
-          '/addReminder': (context) => const AddReminderScreen(),
-        },
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          scaffoldBackgroundColor: Colors.black,
-          appBarTheme: const AppBarTheme(
-            color: Colors.black,
-            toolbarTextStyle: TextStyle(
-              color: Colors.white,
-            ),
-            titleTextStyle: TextStyle(
-              color: Colors.white,
-            ),
-            iconTheme: IconThemeData(
-              color: Colors.white,
-            ),
-          ),
-          iconTheme: const IconThemeData(color: Colors.white),
-          textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-              primary: Colors.blueAccent,
-              textStyle: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ChangeNotifierProvider<TodoListCollection>(
+            create: (BuildContext context) => TodoListCollection(),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              initialRoute: '/',
+              routes: {
+                '/': (context) => HomeScreen(),
+                '/addList': (context) => const AddListScreen(),
+                '/addReminder': (context) => const AddReminderScreen(),
+              },
+              theme: ThemeData(
+                brightness: Brightness.dark,
+                scaffoldBackgroundColor: Colors.black,
+                appBarTheme: const AppBarTheme(
+                  color: Colors.black,
+                  toolbarTextStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  titleTextStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  iconTheme: IconThemeData(
+                    color: Colors.white,
+                  ),
+                ),
+                iconTheme: const IconThemeData(color: Colors.white),
+                textButtonTheme: TextButtonThemeData(
+                  style: TextButton.styleFrom(
+                    primary: Colors.blueAccent,
+                    textStyle: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                dividerColor: Colors.grey[600],
+                colorScheme: ColorScheme.fromSwatch().copyWith(
+                  brightness: Brightness.dark,
+                  secondary: Colors.white,
+                ),
               ),
             ),
-          ),
-          dividerColor: Colors.grey[600],
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            brightness: Brightness.dark,
-            secondary: Colors.white,
-          ),
-        ),
-      ),
+          );
+        }
+
+        return CircularProgressIndicator();
+      },
     );
   }
 }
