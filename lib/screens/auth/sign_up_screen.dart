bce19c19a8611014aa38095a7cae783bcfe32db9
Paul McKillop <pgmckillop@gmail.com>
2022-05-29 14:20:55 +0100

    Lecture 59 End
    Sign In and Sign Up Screens laid out with validation.

--- lib/screens/auth/sign_up_screen.dart
+++ lib/screens/auth/sign_up_screen.dart
@@ -10,6 +10,18 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +31,68 @@ class _SignUpScreenState extends State<SignUpScreen> {
             onPressed: () {
               widget.toggleView();
             },
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
+            icon: const Icon(Icons.person),
+            label: const Text('Sign up'),
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 200.0,
+            ),
+            Text(
+              'Yet Another ToDo List',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter Email'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (val) => val == null || !val.contains('@')
+                          ? 'Enter an email address'
+                          : null,
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration:
+                          const InputDecoration(hintText: 'Enter Password'),
+                      obscureText: true,
+                      validator: (val) => val!.length < 6
+                          ? 'Enter a password of at least 6 characters'
+                          : null,
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print('Submit form');
+                        }
+                      },
+                      child: const Text('Sign Up'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
